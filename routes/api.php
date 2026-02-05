b97d1aceeb9df605bceeb7a10e9da99cbcc80122
DeliaNav <mennavd@gmail.com>
2026-02-05 09:50:16 +0100

    BorrandoCositasInnecesarias

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,34 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+// aqui se importan los controllers
+// use App\Http\Controllers\AuthController;
+// use App\Http\Controllers\BloqueController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+// rutas de autenticacion
+Route::post('/register', [AuthController::class, 'register']); 
+Route::post('/login', [AuthController::class, 'login']); 
+Route::post('/logout', [AuthController::class, 'logout']); 
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// bloques de entrenamiento (crear, borrar, ver)
+Route::get('/bloque', [BloqueController::class, 'index']); 
+Route::post('/bloque/crear', [BloqueController::class, 'store']); 
+Route::get('/bloque/{id}', [BloqueController::class, 'show']); 
+Route::delete('/bloque/{id}/eliminar', [BloqueController::class, 'destroy']); 
+
+// sesiones de entrenamiento (crear, borrar, ver)
+Route::get('/sesion', [SesionController::class, 'index']);
+Route::post('/sesion/crear', [SesionController::class, 'store']); 
+Route::get('/sesion/{id}', [SesionController::class, 'show']); 
+Route::delete('/sesion/{id}', [SesionController::class, 'destroy']); 
+
+// planer de entrenamiento (crear, borrar, ver)
+Route::get('/plan', [PlanController::class, 'index']); 
+Route::post('/plan/crear', [PlanController::class, 'store']); 
+Route::put('/plan/{id}', [PlanController::class, 'update']); 
+Route::delete('/plan/{id}', [PlanController::class, 'destroy']); 
+
+// resultados crear y ver
+Route::post('/resultado/crear', [ResultadoController::class, 'store']); 
+Route::get('/resultado/{id}', [ResultadoController::class, 'show']); 
