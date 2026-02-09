c93a286c3cbaec599f01d242c88aca11616ab4ef
Carla0504 <carla.fernandez864@educa.madrid.org>
2026-02-09 12:10:46 +0100

    cambio ruta css

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="/css/login.css">
+    <link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
-    <div class="contenedor-login">
+    <div class="inicio">
         <h2>Iniciar sesión</h2>
         
         <form id="formulario-login">
--- resources/views/registro.blade.php
+++ resources/views/registro.blade.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registro</title>
-    <link rel="stylesheet" href="/css/registro.css">
+    <link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
-    <div class="contenedor-registro">
+    <div class="inicio">
         <h2>Registro de usuario</h2>
         
         <form id="formulario-registro">
