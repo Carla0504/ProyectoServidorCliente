bb9bc1b61449e5d764d64b60d03b3481d1c6d07c
Carla0504 <carla.fernandez864@educa.madrid.org>
2026-02-09 09:28:50 +0100

    añadirLogin(html)

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,6 +6,16 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <div class="contenedor-login">
+        <h2>Iniciar sesión</h2>
+        <form id="formulario-login">
+            <input type="text" id="usuario" placeholder="Email o usuario" require>
+            <input type="password" id="password" placeholder="Contraseña" require>
+
+            <button type="submit">Enviar</button>
+        </form>
+
+        <div class="mensaje" id="mensaje"></div>
+    </div>
 </body>
 </html>
