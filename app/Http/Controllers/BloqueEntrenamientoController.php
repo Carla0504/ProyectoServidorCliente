1444e50c17a48143152d35055ab5f59fa1697ef7
DeliaNav <mennavd@gmail.com>
2026-02-09 13:15:00 +0100

    validacion Bloque

--- app/Http/Controllers/BloqueEntrenamientoController.php
+++ app/Http/Controllers/BloqueEntrenamientoController.php
@@ -6,8 +6,21 @@ use Illuminate\Http\Request;
 
 class BloqueEntrenamientoController extends Controller
 {
-    //
+    //validacion (hayq ue hacerla no la quites)
     public function create(Request $request){
+        $data = $request->validate([
+            'nombre' => 'required|string|max:255',
+            'descripcion' => 'required|string|max:255',
+            'tipo' => 'required|string|max:255',
+            'duracion'=> 'required', //no se validar ints miralo
+            'potencia_pct_min'=>'required',
+            'potencia_pct_max'=>'required',
+            'pulso_reserva_pct'=>'required',
+            'comentario' => 'required',//en el coso de laravel pone que en body se pone asi asique suponemos que aqui tambien
+        ]);
+
+        if ($data->fails()) return response()->json($data->errors(), 400);
+
         $data => BloqueEntrenamiento::create([
             'nombre' => $request->nombre,
             'descripcion' => $request->descripcion,
@@ -20,5 +33,9 @@ class BloqueEntrenamientoController extends Controller
         ]);
 
         return response()->json(['message'=>'Bloque creado']);
+    }
+
+    public function listDetails(){
+        $data => BloqueEntrenamiento::all();
     }
 }
