2fecaefc5b592913243160b274e0c21e3fea8d0f
DeliaNav <mennavd@gmail.com>
2026-02-10 12:29:17 +0100

    rutas web cambio

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,14 @@ Route::get('/', function () {
   return view('login');
 });
 
+Route::get('/principal', function () {
+
+  return view('principal');
+});
+
+Route::get('/registro', function () {
+
+  return view('registro');
+});
+
+
