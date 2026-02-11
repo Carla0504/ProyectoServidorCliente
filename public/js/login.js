77b330c2dcd0aa6fa840b0d258fecf9b6ed91cf0
Carla0504 <carla.fernandez864@educa.madrid.org>
2026-02-11 12:25:02 +0100

    añadido fetch

--- public/js/login.js
+++ public/js/login.js
@@ -13,14 +13,36 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     function validarCredenciales() {
-        let usuario = document.getElementById('usuario').value;
-        let password = document.getElementById('password').value;
+        let datosUsuario = {
+            usuario: document.getElementById('usuario').value,
+            password: document.getElementById('password').value,
+        };
+        
+        fetch("route/api.php", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            }, 
+            body: JSON.stringify(datosUsuario)
+        })
+        
+        .then(response => {
+            return response.json();
+        })
 
-        if ((usuario === 'admin' || usuario === 'admin@gmail.com') && password === '1234') {
-            window.location.href = '/principal';
-        } else {
-            alert('Se han metido mal las credenciales');
-        }
+        .then(data => {
+            console.log("Respuesta del servidor:", data);
+
+            if ((data.usuario === datosUsuario.usuario || data.usuario === datosUsuario.email) && data.password === datosUsuario.password) {
+                window.location.href = '/principal';
+            } else {
+                alert('Se han metido mal las credenciales');
+            }
+        })
+
+        .catch(error => {
+            console.error("Error al validar las credenciales:", error);
+        });
     }
 
     function redirigirRegistro() {
