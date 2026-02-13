202397c3b6619185bf71a39a2cc8b43800eb9afb
Carla0504 <carla.fernandez864@educa.madrid.org>
2026-02-13 09:55:48 +0100

    muestra los bloques (no comprobado)

---
+++ public/js/bloque.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let bloque = document.getElementById('bloque');
+    
+    bloque.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        fetch('/api/bloque',{
+                method:'POST',
+                headers:{
+                    'Content-Type': 'application/json',
+                    'Accept':'application/json'
+                },
+                body: JSON.stringify({})
+            })
+            .then(function(response){
+                return response.json().then(function(data){
+                    if(response.ok){
+                        localStorage.setItem('token', data.access_token);//aportacion de Henry
+                        window.location.href='/principal';                    
+                    }else{
+                        alert(data.message || 'Error');
+                    }
+                })
+            })
+            .catch(function(error){
+                console.error('Error:', error);
+                alert('No se ha podido conectar :(');
+            })
+    });
+});
