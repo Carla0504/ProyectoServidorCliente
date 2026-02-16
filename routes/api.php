3054d9dd89a27e9b0672c8cc113df55b853d9f26
DeliaNav <mennavd@gmail.com>
2026-02-16 18:54:14 +0100

    Commit Henry: cambios de rutas

--- routes/api.php
+++ routes/api.php
@@ -6,26 +6,39 @@ use Illuminate\Support\Facades\Route;
 // use App\Http\Controllers\ControllerQueSea;
 use App\Http\Controllers\CiclistaController;
 use App\Http\Controllers\BloqueEntrenamientoController;
+use App\Http\Controllers\PlanEntrenamientoController;
 use App\Http\Controllers\SesionEntrenamientoController;
+use App\Http\Controllers\ResultadoEntrenamientoController;
+use App\Http\Controllers\SesionPlanEntrenamientoController;
 
 // rutas de autenticacion
 Route::post('/register', [CiclistaController::class, 'register']); 
 Route::post('/login', [CiclistaController::class, 'login']); 
 Route::post('/logout', [CiclistaController::class, 'logout']); 
 
-// bloques de entrenamiento (crear, borrar, ver)
+// bloques de entrenamiento (crear, borrar, ver, listar)
 Route::get('/bloque', [BloqueEntrenamientoController::class, 'listDetails']); 
 Route::post('/bloque/crear', [BloqueEntrenamientoController::class, 'create']); 
 Route::get('/bloque/{id}', [BloqueEntrenamientoController::class, 'get']); 
 Route::delete('/bloque/{id}/eliminar', [BloqueEntrenamientoController::class, 'destroy']); 
 
+// planes de entrenamiento (crear, borrar, modificar, listar)
+Route::get('/plan', [PlanEntrenamientoController::class, 'listDetails']);
+Route::post('/plan/crear', [PlanEntrenamientoController::class, 'create']); 
+Route::put('/plan/{id}', [PlanEntrenamientoController::class, 'update']); 
+Route::delete('/plan/{id}', [PlanEntrenamientoController::class, 'destroy']); 
+
 // sesiones de entrenamiento (crear, borrar, ver)
 Route::get('/sesion', [SesionEntrenamientoController::class, 'listDetails']);
 Route::post('/sesion/crear', [SesionEntrenamientoController::class, 'create']); 
 Route::get('/sesion/{id}', [SesionEntrenamientoController::class, 'get']); 
-Route::delete('/sesion/{id}', [SesionEntrenamientoController::class, 'destroy']); 
+Route::delete('/sesion/{id}', [SesionEntrenamientoController::class, 'destroy']);
 
-//sesion-plan entrenamiento
-Route::get('/sesionbloque', [BloqueEntrenamientoController::class, 'listDetails']);
-Route::post('/sesionbloque/crear', [BloqueEntrenamientoController::class, 'create']); 
-Route::delete('/sesionbloque/{id}', [BloqueEntrenamientoController::class, 'destroy']);
+// resultado de entrenamiento (ver, crear)
+Route::get('/resultado/crear', [ResultadoEntrenamientoController::class, 'listDetails']);
+Route::get('/resultado/{id}', [ResultadoEntrenamientoController::class, 'create']);
+
+//sesion-plan entrenamiento (listar, crear, borrar)
+Route::get('/sesionbloque', [SesionPlanEntrenamientoController::class, 'listDetails']);
+Route::post('/sesionbloque/crear', [SesionPlanEntrenamientoController::class, 'create']); 
+Route::delete('/sesionbloque/{id}', [SesionPlanEntrenamientoController::class, 'destroy']);
