7d27ecfdeadb6092fc6644319076e3adbc10ba78
Carla0504 <carla.fernandez864@educa.madrid.org>
2026-02-17 12:05:30 +0100

    cambio controller bloque

--- app/Http/Controllers/BloqueEntrenamientoController.php
+++ app/Http/Controllers/BloqueEntrenamientoController.php
@@ -6,33 +6,33 @@ use Illuminate\Http\Request;
 
 class BloqueEntrenamientoController extends Controller
 {
+    // listar todos
+    public function listDetails(){
+        $data = BloqueEntrenamiento::all();
+
+        return response()->json($data);
+    }
+
     //validacion (hayq ue hacerla no la quites)
     public function create(Request $request){
         $data = $request->validate([
             'nombre' => 'required|string|max:255',
             'descripcion' => 'required|string|max:255',
             'tipo' => 'required|string|max:255',
-            'duracion'=> 'required', //no se validar ints miralo
-            'potencia_pct_min'=>'required',
-            'potencia_pct_max'=>'required',
-            'pulso_reserva_pct'=>'required',
-            'comentario' => 'required',//en el coso de laravel pone que en body se pone asi asique suponemos que aqui tambien
+            'duracion_estimada'=> 'required', //no se validar ints miralo
+            'potencia_pct_min'=>'required|numeric',
+            'potencia_pct_max'=>'required|numeric',
+            'pulso_pct_max'=>'required|numeric',
+            'pulso_reserva_pct'=>'required|numeric',
+            'comentario' => 'required|string|max:255',//en el coso de laravel pone que en body se pone asi asique suponemos que aqui tambien
         ]);
 
-        if ($data->fails()) return response()->json($data->errors(), 400);
+        $bloque = BloqueEntrenamiento::create($data);
 
-        $data = BloqueEntrenamiento::create([
-            'nombre' => $request->nombre,
-            'descripcion' => $request->descripcion,
-            'tipo' => $request->tipo,
-            'duracion' => $request->duracion_estimada,
-            'potencia_pct_min' => $request->potencia_pct_min,
-            'potencia_pct_max' => $request->potencia_pct_max,
-            'pulso_reserva_pct' => $request->pulso_reserva_pct,
-            'comentario' => $request->comentario,
-        ]);
-
-        return response()->json(['message'=>'Bloque creado']);
+        return response()->json([
+            'message'=>'Bloque creado correctamente',
+            'data' => $bloque 
+        ], 201);
     }
 
     public function get($id) {
@@ -42,12 +42,6 @@ class BloqueEntrenamientoController extends Controller
             $data = BloqueEntrenamiento::query()->
                 where('id', $id)->
                 get();
-
-        return response()->json($data);
-    }
-
-    public function listDetails(){
-        $data = BloqueEntrenamiento::all();
 
         return response()->json($data);
     }
